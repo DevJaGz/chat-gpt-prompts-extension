52c970933e81975b5df0ff8a51abf4d8211b871d
Julian Andres Gomez Gomez <jgomezpersonal7@gmail.com>
2023-10-28 16:32:10 -0500

    Fix broken image in header

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public",
-          dest: "./",
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   build: {
     outDir,
     emptyOutDir: true,
