2a1d5bf99eb12775c48d709317ec3403a6573488
Julian Andres Gomez Gomez <jgomezpersonal7@gmail.com>
2023-10-28 16:01:18 -0500

    Remove icons form assets

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
-const outDir = resolve(__dirname, 'dist');
+const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,8 +12,12 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        popup: resolve(__dirname, 'src', 'popup' , 'index.html'),
-      }
+        popup: resolve(__dirname, "src", "popup", "index.html"),
+      },
+      output: {
+        chunkFileNames: "[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
     },
   },
-})
+});
