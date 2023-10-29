7999142a8ea319caa4da9d336ebf01710a6beed3
Julian Andres Gomez Gomez <jgomezpersonal7@gmail.com>
2023-10-28 21:44:09 -0500

    Create multiple dummy prompts

--- src/popup/components/Prompts.jsx
+++ src/popup/components/Prompts.jsx
@@ -2,9 +2,33 @@ import Prompt from "./Prompt";
 
 function Prompts() {
   return (
-    <ul className="mt-6">
-      <li>
-      <Prompt />
+    <ul className="mt-6 flex flex-col gap-2 pb-8 max-h-[600px] overflow-y-auto ">
+      <li className="pr-2">
+        <Prompt />
+      </li>
+      <li className="pr-2">
+        <Prompt />
+      </li>{" "}
+      <li className="pr-2">
+        <Prompt />
+      </li>{" "}
+      <li className="pr-2">
+        <Prompt />
+      </li>{" "}
+      <li className="pr-2">
+        <Prompt />
+      </li>{" "}
+      <li className="pr-2">
+        <Prompt />
+      </li>{" "}
+      <li className="pr-2">
+        <Prompt />
+      </li>{" "}
+      <li className="pr-2">
+        <Prompt />
+      </li>{" "}
+      <li className="pr-2">
+        <Prompt />
       </li>
     </ul>
   );
