b220690641b41e72edae0a30e344e62887f75234
Julian Andres Gomez Gomez <jgomezpersonal7@gmail.com>
2023-10-28 21:27:40 -0500

    Set detail summay tags in the prompt

--- src/popup/components/Prompt.jsx
+++ src/popup/components/Prompt.jsx
@@ -1,6 +1,6 @@
 function Prompt() {
   return (
-    <div className="rounded p-2 bg-primary-800">
+    <div className="rounded py-2 px-3 bg-primary-800">
       <header className="flex justify-between items-center flex-nowrap gap-1">
         <h4 className=" font-bold text-ellipsis overflow-hidden whitespace-nowrap max-w-xs text-primary-50">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
@@ -10,14 +10,12 @@ function Prompt() {
           10/01/2023
         </small>
       </header>
-      <main className="mt-2">
-        <p className="text-primary-100 h-24">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          voluptatibus.
-        </p>
-      </main>
+      <details className="max-h-32">
+        <summary>Click here to see the prompt</summary>
+        <p className="text-base">Collapsible content...</p>
+      </details>
       <footer className="mt-4 flex gap-4">
-        <button className="confirmation-btn text-sm">Copy</button>
+        <button className="confirmation-btn text-sm">Load to Chat</button>
         <button className="default-btn text-sm">Edit</button>
         <button className="ml-auto default-btn text-sm">Save</button>
       </footer>
