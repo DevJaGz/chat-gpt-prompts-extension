2a649a9bad34eccf9f04c122957fd0e410bbfc75
Julian Andres Gomez Gomez <jgomezpersonal7@gmail.com>
2023-11-01 22:19:58 -0500

    Add animation to the header in the popup

--- src/popup/components/Header.jsx
+++ src/popup/components/Header.jsx
@@ -2,8 +2,8 @@ function Header() {
 
   return (
     <header className="flex items-center gap-2 p-2 bg-primary-900 ">
-      <img src="/icons/32.png" width="32" height="32" alt="logo" />
-      <h1 className="font-semibold">Chat GPT Prompts</h1>
+      <img src="/icons/32.png" width="32" height="32" alt="logo" className="animate-fade-right animate-duration-500" />
+      <h1 className="font-semibold animate-fade-right animate-duration-500">Chat GPT Prompts</h1>
     </header>
   );
 }
